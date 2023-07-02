cfb47d01f8fef2ad8e8a7774090253c50b7dba6f
nikachu404 <yasinskaa.veronika@gmail.com>
2023-07-02 11:53:09 +0300

    createdAt field

--- src/models/streamer.js
+++ src/models/streamer.js
@@ -6,6 +6,7 @@ const streamerSchema = new mongoose.Schema({
   upvotes: { type: Number, default: 0 },
   downvotes: { type: Number, default: 0 },
   image: String,
+  createdAt: {type: Date, default: Date.now}
 });
 
 export const Streamer = mongoose.model('Streamer', streamerSchema, 'streamers');

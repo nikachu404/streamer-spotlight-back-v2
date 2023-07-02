afe21902053a97f797e0b2591838687eef99d0a4
nikachu404 <yasinskaa.veronika@gmail.com>
2023-07-02 12:20:11 +0300

    returned an accidentally deleted description

--- src/models/streamer.js
+++ src/models/streamer.js
@@ -3,10 +3,11 @@ import mongoose from 'mongoose';
 const streamerSchema = new mongoose.Schema({
   name: String,
   platform: String,
+  description: String,
   upvotes: { type: Number, default: 0 },
   downvotes: { type: Number, default: 0 },
   image: String,
-  createdAt: {type: Date, default: Date.now}
+  createdAt: { type: Date, default: Date.now },
 });
 
 export const Streamer = mongoose.model('Streamer', streamerSchema, 'streamers');
